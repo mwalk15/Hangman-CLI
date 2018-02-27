1bc90c1aeaa0343bf3fc38cf20c80490bd7b49dd
Mia <mwalk15@outlook.com>
2018-02-27 15:40:49 -0600

    updated comments on functions

--- Word.js
+++ Word.js
@@ -1,28 +1,11 @@
-//* **Word**: Used to create an object representing the current word the user is attempting to guess. This should contain word specific logic and data.
-  //
 
-
-//create an object that holds the word that the user needs to guess
-// if index of letter doesn't exists yell at the user and doc their points down
-//else loop through the word and see if it equals and replace the underscore
-
-
-//require the letter
 var HangmanLetter =require("./letters.js");
 
-//create the word constructor
+//created the word constructor
 var Word = function(word){
-	//the word the user would guess
 	this.word = word;
-
-	//array of the word that is being guessed
 	this.wordArray = this.word.split("");
-
-	//an array the holds all the hangman correct letters and underscore for
-	//letters not yet guessed
 	this.displayWord = [];
-
-	//holds every letter that is guessed
 	this.guessLetters =[];
 
 	//makes displayWord array entirely underscores
@@ -32,7 +15,7 @@ var Word = function(word){
 		}
 	}
 
-	//create a function that checks if the user guessed the right letter
+	//created a function that checks if the user guessed the right letter
 	this.newLetter = function(userLetter) {
 		
 		//every letter the user guesses push the letter
